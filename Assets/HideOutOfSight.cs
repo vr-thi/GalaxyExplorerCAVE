7cdc7c771b67fb87c8903fa82367bbf2043695ca
domin1101 <dominik.winkelbauer@outlook.de>
2017-12-11 16:19:53 +0100

    Adds hints to flystick

---
+++ Assets/HideOutOfSight.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cave;
+
+public class HideOutOfSight : MonoBehaviour {
+
+	private enum State
+	{
+		Hidden,
+		TransitionToVisible,
+		Visible,
+		TransitionToHidden
+	}
+
+	private Collider collider;
+	private State state;
+	private float alpha;
+	public float speed = 0.1f;
+	private LineRenderer[] lineRenderer;
+	private TextMesh[] textMeshes;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<Collider> ();	
+		state = State.Hidden;
+		alpha = 0;
+		textMeshes = GetComponentsInChildren<TextMesh> ();
+		lineRenderer = GetComponentsInChildren<LineRenderer> ();
+		RefreshAlpha ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject cameraHolder = GameObject.Find ("CameraHolder");
+		if (cameraHolder != null) {
+			Ray ray = new Ray(cameraHolder.transform.position, cameraHolder.transform.forward);
+			RaycastHit hit;
+			bool inSight = collider.Raycast (ray, out hit, 1);
+
+			if ((state == State.Hidden || state == State.TransitionToHidden) && inSight) {
+				state = State.TransitionToVisible;
+			} else if ((state == State.Visible || state == State.TransitionToVisible) && !inSight) {
+				state = State.TransitionToHidden;
+			}
+
+			if (state == State.TransitionToHidden) {
+				alpha -= speed * TimeSynchronizer.deltaTime;
+				if (alpha <= 0) {
+					alpha = 0;
+					state = State.Hidden;
+				}				
+				RefreshAlpha ();
+			} else if (state == State.TransitionToVisible) {
+				alpha += speed * TimeSynchronizer.deltaTime;
+				if (alpha >= 1) {
+					alpha = 1;
+					state = State.Visible;
+				}				
+				RefreshAlpha ();
+			}
+		}
+	}
+
+	private void RefreshAlpha() 
+	{
+		foreach (LineRenderer singleLineRenderer in lineRenderer) {
+			singleLineRenderer.material.color = new Color (1, 1, 1, alpha);
+		}
+
+		foreach (TextMesh textMesh in textMeshes) {
+			textMesh.color = new Color (1, 1, 1, alpha);
+		}
+	}
+}
--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -16,12 +16,12 @@ public class InputController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if ((InputSynchronizer.GetKeyUp("right") || InputSynchronizer.GetKeyUp("flystick 1")) && planetIndex < planets.Length - 1)
+		if (ViewController.logDistance == 0 && (InputSynchronizer.GetKeyUp("right") || InputSynchronizer.GetKeyUp("flystick 1")) && planetIndex < planets.Length - 1)
 			viewController.SelectPlanet(planets[++planetIndex], planets);
-		if ((InputSynchronizer.GetKeyUp("left") || InputSynchronizer.GetKeyUp("flystick 3")) && planetIndex > 0)
+		if (ViewController.logDistance == 0 && (InputSynchronizer.GetKeyUp("left") || InputSynchronizer.GetKeyUp("flystick 3")) && planetIndex > 0)
 			viewController.SelectPlanet(planets[--planetIndex], planets);
 		if (InputSynchronizer.GetKeyUp ("down") || InputSynchronizer.GetKeyUp ("flystick 2")) {
-			if (ViewController.logDistance > 0	)
+			if (ViewController.logDistance > 0)
 				viewController.ZoomIn (planets[planetIndex], planets);
 			else
 				viewController.ZoomOut (planets);
